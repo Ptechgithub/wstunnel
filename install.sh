5305f2d3db007b2dce659029707f41a62a1a6a2a
Peyman <118217228+Ptechgithub@users.noreply.github.com>
2023-12-16 23:34:49 +0330

    update install.sh

--- install.sh
+++ install.sh
@@ -462,7 +462,7 @@ read -p "Enter number (default is: 1--> tcp): " choice
         timeout_argument=""
     fi
     argument="wstunnel client -L $connection_type://[::]:$config_port:localhost:$config_port$timeout_argument $use_tls_option://$foreign_ip:$port $tls_sni_argument"
-    echo "-----------Your-Arguments-----------------------------------------"
+    echo "------------Your-Arguments------------"
     echo ""
     echo "$argument"
     echo "--------------------------------------"
